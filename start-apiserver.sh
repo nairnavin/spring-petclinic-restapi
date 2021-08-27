626987e0e90bbf6f15f61c1fde7f1f3a1acfbcfc
nairnavin <navin.a.nair@gmail.com>
2021-08-27 11:35:27 +0530

    Delete start-apiserver.sh

--- start-apiserver.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-cd /home/ubuntu/spring-petclinic-restapi
-if [ -f /home/ubuntu/spring-petclinic-restapi/.env ]; then
-  source /home/ubuntu/spring-petclinic-restapi/.env
-fi
-echo "Received ${DB_IP} and ${DB_PORT}"
-./mvnw spring-boot:run
